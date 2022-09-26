fd6047c952b8c3181cea5c17cf65107c02d5b325
pupil <pupil@ivkhk.ee>
2022-09-26 12:59:34 +0300

    second commit

--- src/jptv21javahello/JPTV21JavaHello.java
+++ src/jptv21javahello/JPTV21JavaHello.java
@@ -4,7 +4,7 @@ package jptv21javahello;
 public class JPTV21JavaHello {
 
     public static void main(String[] args) {
-       System.out.print("JAVA HELLO \n");
+       System.out.println("JAVA HELLO");
     }
     
 }
